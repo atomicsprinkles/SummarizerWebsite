94c745715a974b4f09b610645024baee8204248c
atomicsprinkles <atomicsprinkles1@gmail.com>
2024-01-29 18:14:37 -0600

    The first website upload

--- SummarizerWebsite/settings.py
+++ SummarizerWebsite/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +28,7 @@ SECRET_KEY = 'django-insecure-+q@mwvwo)vg6tt=tt5x2=97gvi@r-vhwt#)bnjh46csrk#s8#l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['atomicsoft.works']
 
 
 # Application definition
@@ -122,6 +124,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
