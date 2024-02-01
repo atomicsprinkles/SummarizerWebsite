034816e358aff9ce67a6d1c0944c25d22cceeea5
atomicsprinkles <155010693+atomicsprinkles@users.noreply.github.com>
2024-01-31 19:01:53 -0600

    Update settings.py

--- SummarizerWebsite/settings.py
+++ SummarizerWebsite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-+q@mwvwo)vg6tt=tt5x2=97gvi@r-vhwt#)bnjh46csrk#s8#l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['tiny-bombolone-696673.netlify.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['atomicsprinkles.pythonanywhere.com']
 
 # Application definition
 
